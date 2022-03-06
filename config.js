8ddaf5e45d2a86068af027c27a53121950a3730d
theanh <theanh@example.com>
2022-03-06 22:06:30 +0700

    chucmung

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Babe';
+const giftUrl = 'https://www.facebook.com/hoaithuong15066';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng babe';
+const contentCard = 'Em xinh như một thiên thần. Cám ơn em vì tất cả, nhân ngày Phụ nữ Việt Nam 8 - 3 anh chúc em luôn xinh đẹp, tràn đầy niềm vui và hạnh phúc';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'oke-e.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
